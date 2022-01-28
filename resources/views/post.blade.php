f4a48597558bb6da07c91f5cc8489b554ba728a4
Erkha <jmtatout@gmail.com>
2022-01-28 16:40:26 +0100

    laracast-episode7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium placeat veritatis facere error ratione deleniti numquam ut optio maxime vitae sed voluptatum pariatur iusto reprehenderit qui perferendis ad, molestiae impedit!</p>
+    </article>
+   <a href="/"> Go Back</a>
+</body>
+
+</html>
