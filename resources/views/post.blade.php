8528c44ff365f700927af2da558f5dfbfff01900
Erkha <jmtatout@gmail.com>
2022-01-30 17:06:55 +0100

    laracast: episode 14-17

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    <article>
-        <h1><?= $post->title ?></h1>
-        <?= $post->body ?>
-    </article>
-    <a href="/"> Go Back</a>
-</body>
-
-</html>
+<x-layout>
+    <x-slot name="content">
+        <article>
+            <h1>{{ $post->title }}</h1>
+            {!! $post->body !!}
+        </article>
+        <a href="/"> Go Back</a>
+    </x-slot>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    <?php foreach ($posts as $post) { ?>
-        <article>
-            <h1>
-                <a href="posts/<?= $post->slug ?>">
-                    <?= $post->title ?>
-                </a>
-            </h1>
-            <?= $post->excerpt ?>
-        </article>
-    <?php } ?>
-</body>
-
-</html>
+<x-layout>
+    <x-slot name="content">
+        @foreach ($posts as $post)
+            <article>
+                <h1>
+                    <a href="posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
+                {{ $post->excerpt }}
+            </article>
+        @endforeach
+    </x-slot>
+</x-layout>
